a5ce233b2f49b10cea288cacd67057229159edb8
Douglas Luman <dluman@douglasjluman.com>
2020-01-20 10:15:00 -0500

    Initial commit

---
+++ src/main/java/practicalone/HelloWorld.java
@@ -0,0 +1,17 @@
+package practicalone;
+
+/** Implements a Java "Hello, World!" program.
+ *
+ * @author Solution Repository
+ */
+public class HelloWorld {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // The following prints a single line to the screen
+    System.out.println("Hello, World!");
+  }
+}
